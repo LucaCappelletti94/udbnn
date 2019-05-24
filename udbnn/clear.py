f6a69d99a334850957dce1cd96243a5e23e2121c
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-05-24 15:14:24 +0200

    Now clear also deletes csvs

--- udbnn/clear.py
+++ udbnn/clear.py
@@ -1,6 +1,10 @@
 from .utils import load_settings
+import os
+from glob import glob
 import shutil
 
 def clear(target:str):
     for dataset in load_settings(target)["datasets"]:
         shutil.rmtree("{target}/{path}/run".format(target=target, path=dataset["path"]))
+    for csv in glob("{target}/**/*.csv", recursive=True):
+        os.remove(csv)
