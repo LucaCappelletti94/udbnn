f6bf2f1b740703c096301cc338b982a9e44d7c00
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-05-24 15:22:33 +0200

    Reduced exponent

--- udbnn/utils/get_batch_sizes.py
+++ udbnn/utils/get_batch_sizes.py
@@ -1,8 +1,8 @@
 import numpy as np
 from .set_seed import set_seed
 
-def get_batch_sizes(resolution:int, size:int, seed:int):
+def get_batch_sizes(resolution:int, size:int, seed:int, base:float=1.25, delta:int=10):
     set_seed(seed)
-    batch_sizes = 2**np.arange(resolution)
+    batch_sizes = base**np.arange(delta, delta+resolution)
     np.random.shuffle(batch_sizes)
     return np.ceil(batch_sizes/np.max(batch_sizes)*size).astype(int)
