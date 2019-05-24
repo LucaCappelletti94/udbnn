a6ff3776274385d8a9868088c7f842ebff393b83
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-05-24 15:57:00 +0200

    Extended model

--- udbnn/model.py
+++ udbnn/model.py
@@ -7,8 +7,8 @@ def model(input_size:int):
     set_seed(42)
     model = Sequential([
         InputLayer(input_shape=(input_size,)),
-        *[Dense(15, activation="selu") for i in range(10)],
-        #Dropout(0.3),
+        *[Dense(20, activation="selu") for i in range(10)],
+        Dropout(0.5),
         #*[Dense(10, activation="relu") for i in range(2)],
         #Dropout(0.3),
         Dense(1, activation="sigmoid")
