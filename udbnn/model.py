f32e9aa8b3f304533cd4eaf9e56da0ab1dd896bd
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-05-24 16:01:17 +0200

    Added weight initializer

--- udbnn/model.py
+++ udbnn/model.py
@@ -1,13 +1,14 @@
 from keras.models import Sequential
 from keras.layers import InputLayer, Dense, Dropout
 from .utils import set_seed, auprc, auroc
+from keras.initializers import RandomNormal
 
 def model(input_size:int):
     """Return a multi-layer perceptron."""
     set_seed(42)
     model = Sequential([
         InputLayer(input_shape=(input_size,)),
-        *[Dense(20, activation="selu") for i in range(10)],
+        *[Dense(20, activation="selu", kernel_initializer=RandomNormal(mean=0, std=1), bias_initializer='zeros') for i in range(10)],
         Dropout(0.5),
         #*[Dense(10, activation="relu") for i in range(2)],
         #Dropout(0.3),
