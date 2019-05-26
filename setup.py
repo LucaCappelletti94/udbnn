6ce1de7792babb7fa61d2c8d8a63edc23cb9eafd
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-05-26 21:38:24 +0200

    Updated dependencies

--- setup.py
+++ setup.py
@@ -83,6 +83,6 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     tests_require=test_deps,
-    install_requires=["keras", "sklearn", "keras_tqdm", "tqdm", "pandas", "notipy_me", "silence_tensorflow", "extra_keras_metrics"],
+    install_requires=["keras", "sklearn", "keras_tqdm", "tqdm", "pandas", "notipy_me", "silence_tensorflow", "extra_keras_metrics", "extra_keras_utils"],
     extras_require=extras,
 )
