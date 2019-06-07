30fdb7f070a52e80902dbe6f6a327398aec11574
Luca Cappelletti <cappelletti.luca94@gmail.com>
2019-06-07 16:16:33 +0200

    Plotting single graphs

--- udbnn/__init__.py
+++ udbnn/__init__.py
@@ -3,5 +3,6 @@ warnings.simplefilter("ignore", category=UserWarning)
 import silence_tensorflow
 from .udbnn import run
 from .clear import clear
+from .collect import collect
 
-__all__ = ["run", "clear"]
+__all__ = ["run", "clear", "collect"]
---
+++ udbnn/collect.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from glob import glob
+from plot_keras_history import plot_history
+import os
+
+def customize_axis(axis):
+    axis.set_xscale("log")
+
+def collect(target:str):
+    histories = list(glob("{target}/**/history.json".format(target=target), recursive=True))
+    dfs = []
+    for history in histories:
+        try:
+            tail = pd.read_json(history).tail(1)
+            tail.index = [int(history.split("/")[-4])]
+            dfs.append(tail)
+        except Exception:
+            print(history)
+            os.remove(history)
+        
+    concat = pd.concat(dfs).sort_index()
+    concat.index.name = "Batch sizes"
+    os.makedirs("history", exist_ok=True)
+    plot_history(concat, customization_callback=customize_axis, path="history", single_graphs=True)
+    os.makedirs("interpolated_history", exist_ok=True)
+    plot_history(concat, interpolate=True, customization_callback=customize_axis, path="interpolated_history", single_graphs=True)
+    
